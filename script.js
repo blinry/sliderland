718b3b880ae1f93bee1af96551afedd1d90977e4
blinry <mail@blinry.org>
2022-04-15 01:44:51 +0200

    More animations and first music

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let cos = Math.cos
 let abs = Math.abs
 let floor = Math.floor
 let sqrt = Math.sqrt
+let random = Math.random
 let PI = Math.PI
 
 let sliders = []
@@ -23,24 +24,45 @@ for (var i = 0; i < n; i++) {
 }
 
 function update() {
-    let t = performance.now() - tStart
-    let phaseLength = 3 * 1000 // milliseconds
+    let phaseLength = 4 * 1000 // milliseconds
     let fadeDuration = 1 * 1000
+
+    let t = performance.now() - tStart - fadeDuration / 2
 
     let formula = document.getElementById("formula").value
 
     if (formula.length > 0) {
         apply(userformula)
     } else {
-        let funcs = [stairHor, stairVer, drop, ball, twosines, rand, empty]
+        let funcs = [
+            loading,
+            stairHor,
+            stairVer,
+            onesine,
+            twosines,
+            twister,
+            drop,
+            rotating,
+            fire,
+            ball,
+            multiball,
+            tunnel,
+            heart,
+            rand,
+            empty,
+        ]
+        //funcs = [ball]
+
+        t = t + funcs.length * phaseLength
+
         let phase = Math.floor((t % (funcs.length * phaseLength)) / phaseLength)
         let phasePosition = t % phaseLength
-        if (phasePosition < fadeDuration) {
-            // let amount = 1 - phasePosition / fadeDuration // linear mix
-            let amount =
-                1 / (1 + Math.exp((0.5 - phasePosition / fadeDuration) * 10))
-            let func = funcs[(phase - 1 + funcs.length) % funcs.length]
-            let func2 = funcs[phase]
+        if (phasePosition > phaseLength - fadeDuration) {
+            let p2 = phasePosition - (phaseLength - fadeDuration)
+            let amount = 1 / (1 + Math.exp((0.5 - p2 / fadeDuration) * 10))
+            console.log(amount)
+            let func = funcs[phase]
+            let func2 = funcs[(phase + 1) % funcs.length]
             interpolate(func, func2, amount)
         } else {
             let func = funcs[phase]
@@ -57,7 +79,7 @@ function update() {
 function stairHor(t, i) {
     return (
         floor(
-            (abs((((i + t / 10) / 64) % (2 + 2 * sqrt(2))) - (1 + sqrt(2))) -
+            (abs((((i + t * 100) / 64) % (2 + 2 * sqrt(2))) - (1 + sqrt(2))) -
                 0.5 * sqrt(2)) *
                 8,
         ) / 8
@@ -67,7 +89,7 @@ function stairHor(t, i) {
 function stairVer(t, i) {
     return (
         abs(
-            ((((floor((i / 64) * 8) / 8) * 64 + t / 10) / 64) %
+            ((((floor((i / 64) * 8) / 8) * 64 + t * 100) / 64) %
                 (2 + 2 * sqrt(2))) -
                 (1 + sqrt(2)),
         ) -
@@ -77,8 +99,8 @@ function stairVer(t, i) {
 
 function drop(t, i) {
     return i == 32
-        ? Math.sin(t / 100) * 0.55 + 1
-        : Math.sin(-Math.abs(i / 64 - 0.5) * 10 + t / 100) * 0.03 + 0.5
+        ? Math.sin(t * 10 + PI / 2) * 0.55 + 1
+        : Math.sin(-Math.abs(i / 64 - 0.5) * 10 + t * 10) * 0.03 + 0.5
 }
 
 function empty(t, i) {
@@ -86,46 +108,105 @@ function empty(t, i) {
 }
 
 function apply(func) {
-    let t = performance.now() - tStart
+    let t = (performance.now() - tStart) / 1000
     for (var i = 0; i < sliders.length; i++) {
-        sliders[i].value = func(t, i)
+        let val = func(t, i)
+        if (val !== undefined) {
+            sliders[i].value = val
+        }
     }
 }
 
 function interpolate(func1, func2, amount) {
-    let t = performance.now() - tStart
+    let t = (performance.now() - tStart) / 1000
 
     for (var i = 0; i < sliders.length; i++) {
         sliders[i].value = (1 - amount) * func1(t, i) + amount * func2(t, i)
     }
 }
 
+function rotating(t, i) {
+    let a = sin(t) * PI * 2
+    let x = i / 64 - 0.5
+    let y = (x / cos(a)) * sin(a) + 0.5
+    return y
+}
+
+function banner(t, i) {
+    //IJLMNUVW
+}
+
 function tunnel(t, i) {
     let x = 0.5
     let y = 0.5
 
-    for (let c = 0; c < 4; c++) {
-        let r = 0.1 * c
+    let n = 3
+    let c = floor(i) % n
+    let maxR = 0.5 * sqrt(2)
+    let r = ((maxR / n) * c + t / 2) % maxR
 
-        let j = i / 64
-        if (Math.abs(x - j) <= r && i % 4 == c) {
-            let a = Math.acos(Math.abs(x - j) / r)
-            let offset = Math.sin(a) * r
-            if (i % 2 == 0) {
-                return y + offset
-            } else {
-                return y - offset
-            }
+    let j = i / 64
+    if (Math.abs(x - j) <= r) {
+        let a = Math.acos(Math.abs(x - j) / r)
+        let offset = Math.sin(a) * r
+        if (t % 0.1 > 0.05) {
+            return y + offset
         } else {
-            return 0
+            return y - offset
         }
+    } else {
+        return 0
     }
+}
+
+function loading(t, i) {
+    return -(i - (((64 / 105) * t) % 15) ** 5)
+}
+
+function fire(t, i) {
+    let r = sin(i ** 2 + t * 30) * 0.5 + 0.5
+    let h = sin(t) * 0.2 + 1
+    return r * h * (cos(((i - 32) / 70) * PI) * 0.7 + sin(i + t * 10) * 0.2)
+}
+
+function heart(t, i) {
+    let val1 = 0
+    let val2 = 0
+
+    let j = i / 64 + 0.01
+
+    let r = 0.2 + 0.03 * (sin(t * 4) * 0.5 + 0.5)
+
+    let x = 0.5 - r
+    let y = 0.43 + r
+
+    if (Math.abs(x - j) <= r) {
+        let a = Math.acos(Math.abs(x - j) / r)
+        let offset = Math.sin(a) * r
+        val1 = y + offset
+    }
+
+    x = 0.5 + r
+
+    if (Math.abs(x - j) <= r) {
+        let a = Math.acos(Math.abs(x - j) / r)
+        let offset = Math.sin(a) * r
+        val1 = y + offset
+    }
+
+    r = 1.9 * r
+    dist = Math.abs(0.5 - j)
+    if (dist <= r) {
+        val2 = dist + 0.43 - r / sqrt(2)
+    }
+
+    return i % 2 == 0 ? val1 : val2
 }
 
 function ball(t, i) {
     let r = 0.1
-    let x = Math.sin(t / 1000) * (1 - 2 * r) * 0.5 + 0.5
-    let y = Math.sin(t / 800) * (1 - 2 * r) * 0.5 + 0.5
+    let x = Math.sin(t) * (1 - 2 * r) * 0.5 + 0.5
+    let y = Math.sin(t * 0.8) * (1 - 2 * r) * 0.5 + 0.5
 
     let j = i / 64
     if (Math.abs(x - j) <= r) {
@@ -139,6 +220,32 @@ function ball(t, i) {
     } else {
         return 0 //Math.sin(t / 50) * 0.5 + 0.5
     }
+}
+
+function multiball(t, i) {
+    let lowest = 0
+    for (let c = 0; c < 4; c++) {
+        let r = 0.1 + 0.02 * c
+        let x = Math.sin(t + c) * (1 - 2 * r) * 0.5 + 0.5
+        let y = Math.sin(t * 0.8 + 2 * c) * (1 - 2 * r) * 0.5 + 0.5
+
+        let j = i / 64
+        if (Math.abs(x - j) <= r) {
+            let a = Math.acos(Math.abs(x - j) / r)
+            let offset = Math.sin(a) * r
+            if (i % 2 == 0) {
+                if (lowest == 0 || random() < 0.5) {
+                    lowest = y + offset
+                }
+            } else {
+                if (lowest == 0 || random() < 0.5) {
+                    lowest = y - offset
+                }
+            }
+        } else {
+        }
+    }
+    return lowest
 }
 
 function userformula(t, i) {
@@ -188,12 +295,24 @@ function twosines2(t) {
     }
 }
 
+function onesine(t, i) {
+    return Math.sin(i / 10 + t) * 0.2 + 0.5
+}
+
 function twosines(t, i) {
     if (i % 2 == 0) {
-        return Math.sin(i / 10 + t / 1000) * 0.5 + 0.5
+        return onesine(t, i)
     } else {
-        return Math.sin(i / 30 + t / 800) * 0.5 + 0.5
+        return Math.sin(i / 30 + t * 0.8) * 0.5 + 0.5
     }
+}
+
+function twister(t, i) {
+    let n = 3
+    let offset = i % n
+    let d = sin(i / 10 + t * 0.8) * 0.01
+    let d2 = sin(-i / 10 + t * 2.8) * 0.02
+    return Math.sin(i / 10 + t * 2.8 + (offset / n) * PI) * (0.1 + d2) + 0.5 + d
 }
 
 update()
