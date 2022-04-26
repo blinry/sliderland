647b1686d302ea21736adbc82bf4666eeec3b3bb
blinry <mail@blinry.org>
2022-04-26 12:11:05 +0200

    Give visual feedback on errors

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-let releaseMode = false
-
-if (releaseMode) {
-    document.querySelector("audio").controls = false
-    document.querySelector("#formula").style.display = "none"
-}
-
-let sin = Math.sin
-let cos = Math.cos
-let tan = Math.tan
-let abs = Math.abs
-let floor = Math.floor
-let ceil = Math.ceil
-let sqrt = Math.sqrt
-let random = Math.random
-let max = Math.max
-let min = Math.min
-let PI = Math.PI
-
 let sliders = []
 let n = 64
 
@@ -42,39 +23,19 @@ let phaseLength = (40 / 6) * 1000 // milliseconds
 let fadeDuration = 1 * 1000
 
 function update() {
-    let t = performance.now() - tStart - fadeDuration / 2
+    let formulaText = formula.innerText
 
-    let formula = document.getElementById("formula").innerText
-
-    if (formula.length > 0 || !releaseMode) {
-        let fail = false
-        let eval
-        try {
-            eval = new Function(
-                "t",
-                "i",
-                "x",
-                "try { with (Math) { return " + formula + "}} catch (e) {}",
-            )
-        } catch (e) {
-            fail = true
-        }
-
-        if (fail) {
-            document.querySelector("#formula").classList.add("error")
-        } else {
-            document.querySelector("#formula").classList.remove("error")
-
-            let t = (performance.now() - tStart) / 1000
-            for (var i = 0; i < sliders.length; i++) {
-                let x = i / 63
-                let val = 0
-                try {
-                    val = eval(t, i, x)
-                    console.log(val)
-                } catch (e) {}
-
+    if (formulaText.length > 0) {
+        let t = (performance.now() - tStart) / 1000
+        for (var i = 0; i < sliders.length; i++) {
+            let x = i / 63
+            let val = eval(t, i, x)
+            if (val === undefined || isNaN(val) || !isFinite(val)) {
+                sliders[i].value = 0
+                sliders[i].disabled = true
+            } else {
                 sliders[i].value = val
+                sliders[i].disabled = false
             }
         }
     }
@@ -83,17 +44,49 @@ function update() {
 }
 
 let formula = document.getElementById("formula")
+let eval = () => 0
 formula.oninput = () => {
     // URL-encode formula into hash
     let hash = encodeURIComponent(formula.innerText)
     window.location.hash = hash
+
+    // Reset t
     tStart = performance.now()
+
+    // Extract function
+    updateFormula()
+}
+
+function updateFormula() {
+    try {
+        let formulaText = formula.innerText
+        if (formulaText.length > 0) {
+            eval = new Function(
+                "t",
+                "i",
+                "x",
+                "try { with (Math) { return " +
+                    formulaText +
+                    "}} catch (e) {return undefined}",
+            )
+        } else {
+            for (var i = 0; i < sliders.length; i++) {
+                sliders[i].value = 0
+                sliders[i].disabled = false
+            }
+        }
+        formula.classList.remove("error")
+    } catch (e) {
+        formula.classList.add("error")
+    }
 }
 
 // URL-decode hash
 let hash = decodeURIComponent(window.location.hash.substr(1))
 if (hash !== "") {
-    document.getElementById("formula").innerText = hash
+    formula.innerText = hash
 }
+
+updateFormula()
 
 update()
